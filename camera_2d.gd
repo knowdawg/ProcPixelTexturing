5f7283c62b79b7e9d936cdd84dd37756e380d69c
Nolan Kellerman <nolan@sharkus.com>
2025-10-16 14:49:22 +0200

    small bug fixed to global illumination

--- camera_2d.gd
+++ camera_2d.gd
@@ -1,13 +1,13 @@
 extends Sprite2D
 
-var moveSpeed = 120.0
+var moveSpeed = 4.0
 
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("ui_left"):
-		position.x -= delta * moveSpeed
+		position.x -= moveSpeed
 	if Input.is_action_pressed("ui_right"):
-		position.x += delta * moveSpeed
+		position.x += moveSpeed
 	if Input.is_action_pressed("ui_up"):
-		position.y -= delta * moveSpeed
+		position.y -= moveSpeed
 	if Input.is_action_pressed("ui_down"):
-		position.y += delta * moveSpeed
+		position.y += moveSpeed
--- sun.gd
+++ sun.gd
@@ -3,6 +3,6 @@ extends DirectionalLight2D
 var t = 0.0
 func _process(delta: float) -> void:
 	t += delta
-	#rotation = sin(t)
+	rotation = sin(t)
 	
 	RenderingServer.global_shader_parameter_set("SUN_DIRECTION", rotation - PI/2.0)
